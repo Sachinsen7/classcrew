52e664c193f229f7ba2cecaf61d821adde438278
Sachin Sen <sachinsen1920@gmail.com>
2025-09-03 17:37:59 +0530

    add components

---
+++ class-crew/src/types/course.ts
@@ -0,0 +1,11 @@
+export interface Course {
+  id: string;
+  tagColor: string;
+  tagText: string;
+  category: string;
+  title: string;
+  description: string;
+  tags: string[];
+  date: string;
+  price: string;
+}
