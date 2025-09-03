5ab6e1f43edb309e565818bd41bf031b292c510f
Sachin Sen <sachinsen1920@gmail.com>
2025-09-03 16:42:39 +0530

    add home components

---
+++ class-crew/src/components/home/index.ts
@@ -0,0 +1,5 @@
+export { default as Banner } from "@/components/home/banner/page";
+export { default as HeroSection } from "@/components/home/caraousal/page";
+export { default as NavBar } from "@/components/home/Navbar/page";
+export { default as CourseCard } from "@/components/home/CourseCard/page";
+export { default as CourseSection } from "@/components/home/CourseSection/page";
