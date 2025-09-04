c54041f45ddb110bed3fe4e055302469da70f0b2
Sachin Sen <sachinsen1920@gmail.com>
2025-09-04 15:36:24 +0530

    add main page

---
+++ class-crew/src/app/mypage/layout.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import MyPageLayout from "@/components/My page/layout";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <MyPageLayout>{children}</MyPageLayout>;
+}
