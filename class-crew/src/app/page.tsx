c53b542c33246a7faff2de1c29e51316e3cb1ecc
Sachin Sen <sachinsen1920@gmail.com>
2025-09-04 15:36:05 +0530

    import my page

--- class-crew/src/app/page.tsx
+++ class-crew/src/app/page.tsx
@@ -1,13 +1,17 @@
 "use client";
 
 import StorePage from "@/components/home/page";
+import PaymentManagement from "@/components/My page/PaymentManagement/page";
 import LearningStatus from "@/components/My page/LearningStatus/page";
+import MyPage from "@/components/My page/page";
 
 function page() {
   return (
     <div className="min-h-screen flex items-center justify-center flex-col">
       <StorePage />
-      <LearningStatus />
+      {/* <LearningStatus /> */}
+      {/* <PaymentManagement /> */}
+      <MyPage />
     </div>
   );
 }
