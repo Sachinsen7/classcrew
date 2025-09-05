77b3c6588f439b6b94544028456b999961b2804c
Sachin Sen <sachinsen1920@gmail.com>
2025-09-05 15:59:26 +0530

    add account page

---
+++ class-crew/src/app/mypage/account/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/layout/navbar/page";
+import RequiredInfo from "@/components/My page/AccountSettings/Account_Form/page";
+
+export default function AccountSettingsPage() {
+  return (
+    <div className="min-h-screen">
+      <Navbar />
+      <RequiredInfo />
+    </div>
+  );
+}
