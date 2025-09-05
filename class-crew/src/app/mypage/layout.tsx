f8d38db3eefc76c09eb03556b3b806b6557cdacb
Sachin Sen <sachinsen1920@gmail.com>
2025-09-05 12:44:10 +0530

    add layout

--- class-crew/src/app/mypage/layout.tsx
+++ class-crew/src/app/mypage/layout.tsx
@@ -5,3 +5,4 @@ import MyPageLayout from "@/components/My page/layout";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return <MyPageLayout>{children}</MyPageLayout>;
 }
+
