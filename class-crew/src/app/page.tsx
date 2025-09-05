0c97a782d92efb40e052bac088aac13d6c073689
Sachin Sen <sachinsen1920@gmail.com>
2025-09-05 12:42:10 +0530

    add app file

--- class-crew/src/app/page.tsx
+++ class-crew/src/app/page.tsx
@@ -1,19 +1,12 @@
-"use client";
-
 import StorePage from "@/components/home/page";
 import PaymentManagement from "@/components/My page/PaymentManagement/page";
 import LearningStatus from "@/components/My page/LearningStatus/page";
 import MyPage from "@/components/My page/page";
 
-function page() {
+export default function HomePage() {
   return (
-    <div className="min-h-screen flex items-center justify-center flex-col">
+    <div className="min-h-screen">
       <StorePage />
-      {/* <LearningStatus /> */}
-      {/* <PaymentManagement /> */}
-      <MyPage />
     </div>
   );
 }
-
-export default page;
