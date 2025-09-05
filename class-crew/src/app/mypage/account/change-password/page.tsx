3dd925396a14b17adb24e60b1720b1b93acc69c0
Sachin Sen <sachinsen1920@gmail.com>
2025-09-05 15:59:39 +0530

    add change password

---
+++ class-crew/src/app/mypage/account/change-password/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/layout/navbar/page";
+import ChangePassword from "@/components/My page/AccountSettings/ChangePassword/page";
+
+export default function ChangePasswordRoute() {
+  return (
+    <div className="min-h-screen">
+      <Navbar />
+      <ChangePassword />
+    </div>
+  );
+}
