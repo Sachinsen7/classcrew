71ef72f9fc1572bde9751d6a53b06568c7f10d8e
Sachin Sen <sachinsen1920@gmail.com>
2025-09-05 15:59:59 +0530

    add account settings page

--- class-crew/src/components/My page/AccountSettings/Account_Form/page.tsx
+++ class-crew/src/components/My page/AccountSettings/Account_Form/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import { useState } from "react";
-import { FaChevronUp, FaChevronDown } from 'react-icons/fa';
+import { useRouter } from "next/navigation";
+import { FaChevronUp, FaChevronDown } from "react-icons/fa";
 
 export default function RequiredInfo() {
+  const router = useRouter();
   const [formData, setFormData] = useState({
     email: "",
     userId: "",
@@ -15,7 +17,7 @@ export default function RequiredInfo() {
     phone: "",
     dobYear: "",
     dobMonth: "",
-    dobDay: ""
+    dobDay: "",
   });
 
   const handleChange = (
@@ -29,21 +31,15 @@ export default function RequiredInfo() {
     <>
       <main className="w-[1270px] mt-6 flex flex-col items-center mb-10">
         <div className="w-full border border-[#E6E6E6] rounded-sm">
-
           <div className="bg-[#EEEEEE] px-4 py-3 flex items-center gap-2">
-
             <FaChevronUp className="text-primary" />
-
 
             <span className="text-[16px] font-extrabold text-primary">
               필수정보 <span className="text-[#EE0000]">*</span>
             </span>
           </div>
 
-
-
           <div className="p-6 flex flex-col gap-6 text-[14px]">
-
             <div className=" flex flex-row">
               <label className="block text-[20px] text-primary font-bold mb-2">
                 이메일 주소 <span className="text-[#EE0000]">*</span>
@@ -58,11 +54,10 @@ export default function RequiredInfo() {
               />
             </div>
             <p className="text-[16px] text-[#6D6D6D] ">
-              이메일 주소는 계정 인증, 새 비밀번호 수신 하거나 특정 뉴스 또는 알림을 이메일로 수신하는 경우에
-              사용됩니다. 반드시 수신 가능한 이메일을 적어 주세요.
+              이메일 주소는 계정 인증, 새 비밀번호 수신 하거나 특정 뉴스 또는
+              알림을 이메일로 수신하는 경우에 사용됩니다. 반드시 수신 가능한
+              이메일을 적어 주세요.
             </p>
-
-
 
             <div className=" flex flex-row">
               <label className="block text-[20px] text-primary font-bold mb-2">
@@ -78,11 +73,9 @@ export default function RequiredInfo() {
               />
             </div>
             <p className="text-[16px] text-[#6D6D6D] ">
-              공백, 마침표(.), 하이픈(-), 어포스트로피(&apos;), 밑줄(_), @ 부호를 포함하여 여러 특수 문자가
-              허용됩니다.
+              공백, 마침표(.), 하이픈(-), 어포스트로피(&apos;), 밑줄(_), @
+              부호를 포함하여 여러 특수 문자가 허용됩니다.
             </p>
-
-
 
             <div className=" flex flex-row">
               <label className="block text-[20px] text-primary font-bold mb-2">
@@ -98,6 +91,7 @@ export default function RequiredInfo() {
                 />
                 <button
                   type="button"
+                  onClick={() => router.push("/mypage/account/change-password")}
                   className="bg-black w-[256px] h-[60px] text-white px-4 py-2 rounded-md text-sm"
                 >
                   비밀번호 변경하기
@@ -105,8 +99,6 @@ export default function RequiredInfo() {
               </div>
             </div>
             <p className="text-[16px] text-[#6D6D6D] ">비밀번호의 강도:</p>
-
-
 
             <div className=" flex flex-row">
               <label className="block text-[20px] text-primary font-bold mb-2">
@@ -121,12 +113,14 @@ export default function RequiredInfo() {
               />
             </div>
             <p className="text-[16px] text-[#6D6D6D] ">
-              새로운 계정에 이용할 새로운 비밀번호를 두 입력 필드에 동일하게 입력하십시오.
+              새로운 계정에 이용할 새로운 비밀번호를 두 입력 필드에 동일하게
+              입력하십시오.
             </p>
 
-
             <div className=" flex flex-row">
-              <label className="block text-[20px] text-primary font-bold mb-2">성명 <span className="text-[#EE0000]">*</span></label>
+              <label className="block text-[20px] text-primary font-bold mb-2">
+                성명 <span className="text-[#EE0000]">*</span>
+              </label>
               <input
                 type="text"
                 name="name"
@@ -136,7 +130,6 @@ export default function RequiredInfo() {
               />
             </div>
 
-
             <div className=" flex flex-row">
               <label className="block text-[20px] text-primary font-bold mb-2">
                 성별 <span className="text-[#EE0000]">*</span>
@@ -145,7 +138,6 @@ export default function RequiredInfo() {
               <div className="relative w-[565px]">
                 <select
                   name="gender"
-                 
                   value={formData.gender}
                   onChange={handleChange}
                   className="w-[565px] h-[60px] border border-[#DDDDDD] ml-25 px-3 py-2 rounded-sm appearance-none focus:outline-none focus:ring-2 focus:ring-primary"
@@ -161,8 +153,6 @@ export default function RequiredInfo() {
                 </span>
               </div>
             </div>
-
-
 
             <div className=" flex flex-row">
               <label className="block text-[20px] text-primary font-bold mb-2">
@@ -189,32 +179,26 @@ export default function RequiredInfo() {
             </div>
 
             <p className="text-[16px] text-[#6D6D6D] mt-3">
-              * 재직자 및 기업교육 담당자는 과정신청 시 다수의 교육생 정보 입력이 가능합니다
+              * 재직자 및 기업교육 담당자는 과정신청 시 다수의 교육생 정보
+              입력이 가능합니다
             </p>
           </div>
         </div>
-
       </main>
 
       {/* 2nd*/}
 
       <main className="w-[1270px] mt-6 flex flex-col items-center mb-10">
         <div className="w-full border border-[#E6E6E6] rounded-sm">
-
           <div className="bg-[#EEEEEE] px-4 py-3 flex items-center gap-2">
-
             <FaChevronUp className="text-primary" />
-
 
             <span className="text-[16px] font-extrabold text-primary">
               부가정보 <span className="text-[#EE0000]">*</span>
             </span>
           </div>
 
-
-
           <div className="p-6 flex flex-col gap-6 text-[14px]">
-         
             <div className=" flex flex-row">
               <label className="block text-[20px] text-primary font-bold mb-2">
                 휴대전화
@@ -225,23 +209,20 @@ export default function RequiredInfo() {
                 value={formData.phone}
                 onChange={handleChange}
                 className="w-[565px] h-[60px] border border-[#DDDDDD] ml-18 px-3 py-2 rounded-sm"
-
               />
             </div>
             <p className="text-[16px] text-[#6D6D6D] ">
-              이메일 주소는 계정 인증, 새 비밀번호 수신 하거나 특정 뉴스 또는 알림을 이메일로 수신하려는 경우에 사용됩니다. 반드시 수신 가능한 이메일을 적어 주세요.
+              이메일 주소는 계정 인증, 새 비밀번호 수신 하거나 특정 뉴스 또는
+              알림을 이메일로 수신하려는 경우에 사용됩니다. 반드시 수신 가능한
+              이메일을 적어 주세요.
             </p>
 
-
-            
             <div className="flex items-center gap-6">
-             
               <label className="block text-[20px] text-primary font-bold whitespace-nowrap">
                 생년월일
               </label>
 
               <div className="flex flex-row gap-4 ml-11">
-             
                 <div className="relative w-[160px]">
                   <select
                     name="dobYear"
@@ -249,7 +230,9 @@ export default function RequiredInfo() {
                     onChange={handleChange}
                     className="w-full h-[60px] border border-[#DDDDDD] px-3 py-2 rounded-sm appearance-none focus:outline-none focus:ring-2 focus:ring-primary"
                   >
-                    <option value="" className="font-bold">년도</option>
+                    <option value="" className="font-bold">
+                      년도
+                    </option>
                     {Array.from({ length: 26 }, (_, i) => {
                       const year = 2000 + i;
                       return (
@@ -271,7 +254,9 @@ export default function RequiredInfo() {
                     onChange={handleChange}
                     className="w-full h-[60px] text-bold border border-[#DDDDDD] px-3 py-2 rounded-sm appearance-none focus:outline-none focus:ring-2 focus:ring-primary"
                   >
-                    <option value="" className="font-bold">월</option>
+                    <option value="" className="font-bold">
+                      월
+                    </option>
                     {Array.from({ length: 12 }, (_, i) => (
                       <option key={i + 1} value={i + 1}>
                         {i + 1}
@@ -290,7 +275,9 @@ export default function RequiredInfo() {
                     onChange={handleChange}
                     className="w-full h-[60px] border border-[#DDDDDD] px-3 py-2 rounded-sm appearance-none focus:outline-none focus:ring-2 focus:ring-primary"
                   >
-                    <option value="" disabled  className="font-bold">일</option>
+                    <option value="" disabled className="font-bold">
+                      일
+                    </option>
                     {Array.from({ length: 31 }, (_, i) => (
                       <option key={i + 1} value={i + 1}>
                         {i + 1}
@@ -303,48 +290,56 @@ export default function RequiredInfo() {
                 </div>
               </div>
             </div>
-
           </div>
         </div>
 
         <div className="w-full space-y-4 mt-8">
-
           <p className="text-[20px] font-bold text-black">
-            이용약관, 개인정보 수집 및 이용, 메일링 서비스(선택)에 모두 동의 합니다.
+            이용약관, 개인정보 수집 및 이용, 메일링 서비스(선택)에 모두 동의
+            합니다.
           </p>
-
 
           <div className="flex flex-col gap-4 mt-4 text-[20px] text-black font-medium">
             <label className="flex items-center gap-2">
-              <input type="checkbox" className="w-5 h-5 border border-gray-400" />
+              <input
+                type="checkbox"
+                className="w-5 h-5 border border-gray-400"
+              />
               <span>모두 동의</span>
             </label>
 
             <label className="flex items-center gap-2">
-              <input type="checkbox" className="w-5 h-5 border border-gray-400" />
+              <input
+                type="checkbox"
+                className="w-5 h-5 border border-gray-400"
+              />
               <span>
                 이용약관 동의 <span className="text-[#EE0000]">*</span>
               </span>
             </label>
 
             <label className="flex items-center gap-2">
-              <input type="checkbox" className="w-5 h-5 border border-gray-400" />
+              <input
+                type="checkbox"
+                className="w-5 h-5 border border-gray-400"
+              />
               <span>
                 개인정보 수집 및 이용 <span className="text-[#EE0000]">*</span>
               </span>
             </label>
 
             <label className="flex items-center gap-2">
-              <input type="checkbox" className="w-5 h-5 border border-gray-400" />
+              <input
+                type="checkbox"
+                className="w-5 h-5 border border-gray-400"
+              />
               <span>메일링서비스</span>
             </label>
           </div>
 
-
           <button className="w-full h-[55px] bg-black text-white text-[20px] font-bold py-4 rounded-full mt-6">
             가입하기
           </button>
-
 
           <div className="flex justify-center gap-6 mt-6">
             <button className="w-[256px] h-[50px] bg-gray-100 text-black text-[24px] font-bold rounded">
@@ -355,7 +350,6 @@ export default function RequiredInfo() {
             </button>
           </div>
         </div>
-
       </main>
     </>
   );
