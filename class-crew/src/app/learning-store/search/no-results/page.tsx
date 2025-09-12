d6046f9649d7fa0a992e9abe0ff27e4eb5115783
Sachin Sen <sachinsen1920@gmail.com>
2025-09-12 16:14:00 +0530

    add no results page

---
+++ class-crew/src/app/learning-store/search/no-results/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import React from "react";
+import NoSearchResults from "@/components/My page/LearningCrewStore/NoSearchResults/page";
+
+export default function Page() {
+  return <NoSearchResults />;
+}
