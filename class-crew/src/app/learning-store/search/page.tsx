1f43a797a53b5a4d5b4bb2595a4c6f4745f0bac6
Sachin Sen <sachinsen1920@gmail.com>
2025-09-12 16:12:57 +0530

    add search page

---
+++ class-crew/src/app/learning-store/search/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import React from "react";
+import SearchResults from "@/components/My page/LearningCrewStore/SearchResults/page";
+
+export default function Page() {
+  return <SearchResults />;
+}
