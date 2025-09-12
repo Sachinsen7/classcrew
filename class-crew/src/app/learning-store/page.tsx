ef3cd0fbf569f6d44d7297438a645a001da0237f
Sachin Sen <sachinsen1920@gmail.com>
2025-09-12 16:12:21 +0530

    add main page learningstore

---
+++ class-crew/src/app/learning-store/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import React from "react";
+import StoreGrid from "@/components/My page/LearningCrewStore/store/page";
+
+export default function Page() {
+  return <StoreGrid />;
+}
