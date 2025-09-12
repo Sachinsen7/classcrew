23020c4c51c5408e293691748df79246b3e15b5f
sapeksh <sapekshvishwakarma@gmail.com>
2025-09-12 16:45:44 +0530

    update coursehistory list page

--- class-crew/src/app/customerservicecenter/checkcoursehistory/coursehistorylist/page.tsx
+++ class-crew/src/app/customerservicecenter/checkcoursehistory/coursehistorylist/page.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Coursehistorylist from "@/components/CustomerserviceCenter/CheckCourseHistory/coursehistorylist/page"
+import Navbar from '@/components/layout/navbar/page'
 
 function page() {
   return (
-    <div>xxxxxxxxxxxxxxxxx</div>
+    <>
+    <Navbar/>
+    <Coursehistorylist/>
+    </>
   )
 }
 
