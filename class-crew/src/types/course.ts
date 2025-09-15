b98bb7af49ba0fa019b23b39a39d40d4688b563d
Sachin Sen <sachinsen1920@gmail.com>
2025-09-15 11:40:34 +0530

    fix typos in course ts fle deployment error

--- class-crew/src/types/course.ts
+++ class-crew/src/types/course.ts
@@ -1,18 +1,16 @@
-
-
 export interface Course {
   id: string;
   title: string;
   description: string;
-  price: number;      
-  priceText?: string; 
-  date: Date;         
+  price: string | number;
+  priceText?: string;
+  date: string | Date;
   category: string;
   tagText: string;
   tagColor: string;
   tags: string[];
-  image: string;     
-  target: string;    
-  duration: string;   
-  location: string;   
+  image?: string;
+  target?: string;
+  duration?: string;
+  location?: string;
 }
