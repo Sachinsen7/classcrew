c9c6538c9679ec01f1dc7a47aa6dab5f5a93d55b
Sachin Sen <sachinsen1920@gmail.com>
2025-09-22 11:24:49 +0530

    fix deployment issues

--- class-crew/src/components/layout/navbar/page.tsx
+++ class-crew/src/components/layout/navbar/page.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { useState } from "react";
-import { Menu, X, Mail } from "lucide-react";
+import { Menu, X } from "lucide-react";
 import { phenomena } from "@/app/font";
 import { motion } from "framer-motion";
 
