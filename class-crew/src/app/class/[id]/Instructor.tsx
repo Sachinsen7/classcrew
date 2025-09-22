b6315ac2e07321a07708d5e6b0160876d1570299
Sachin Sen <sachinsen1920@gmail.com>
2025-09-22 10:44:24 +0530

    fix instructor search bar

--- class-crew/src/app/class/[id]/Instructor.tsx
+++ class-crew/src/app/class/[id]/Instructor.tsx
@@ -4,6 +4,8 @@ import React from "react";
 import SearchBanner from "@/components/ui/SearchBanner";
 
 export default function ClassInstructor() {
+  const [theme, setTheme] = React.useState("");
+  const [job, setJob] = React.useState("");
   return (
     <main className="w-[1270px] mt-10 mb-20">
       <div className="w-full space-y-14">
@@ -14,7 +16,9 @@ export default function ClassInstructor() {
         <h3 className="text-[36px] font-extrabold text-black">정상 범</h3>
 
         <section className="">
-          <h4 className="text-[28px] font-semibold ml-6 text-black">학력 및 경력</h4>
+          <h4 className="text-[28px] font-semibold ml-6 text-black">
+            학력 및 경력
+          </h4>
           <ul className="space-y-5 text-[25px] text-[#6D6D6D] font-normal ml-10 mt-4">
             <li>- 고려대학교 대학원 교육학 석사(우수논문상)</li>
             <li>- 고려대학교 대학원 교육학 박사 수료</li>
@@ -49,25 +53,30 @@ export default function ClassInstructor() {
 
         <section className="space-y-4">
           <h4 className="text-[28px] font-semibold text-black">출강이력</h4>
-             <ul className="space-y-5 text-[25px] text-[#6D6D6D] font-normal ml-6 mt-4">
-            <li>- 삼성반도체, 삼성SDS, 삼성병원, 현대자동차인재개발원, 현대자동차,
+          <ul className="space-y-5 text-[25px] text-[#6D6D6D] font-normal ml-6 mt-4">
+            <li>
+              - 삼성반도체, 삼성SDS, 삼성병원, 현대자동차인재개발원, 현대자동차,
               현대모비스, 현대제철, 현대중공업, 현대삼호중공업, LIG넥스원,
               한국항공우주산업, CJ제일제당, 불로그룹코리아, 기아자동차,
-              농심그룹, 대우건설, LG전자, LG화학, GS글로벌 외 다수</li>
-           </ul>
-        
+              농심그룹, 대우건설, LG전자, LG화학, GS글로벌 외 다수
+            </li>
+          </ul>
         </section>
 
         {/* Bottom Banner */}
         <SearchBanner
-          className="mt-20"
+          titleDiv="pl-3 flex"
+          className="mt-32 px-5"
           bgImage="/images/Block_with_illustration.png"
           title="나를 위한 투자, 지금 이 CLASS로 시작하세요"
           description="성장을 위한 꾸준한 노력, 이미 당신은 능력자!"
           buttonText="CLASS 신청하기"
           width="w-[1245px]"
           height="h-[147px]"
-          onSearch={() => console.log("Apply clicked")}
+          buttonWidth="w-[210px]"
+          buttonHeight="h-[53px]"
+          // buttonLink="/payments"
+          onSearch={() => console.log("Searching with:", theme, job)}
           filters={[]}
         />
       </div>
