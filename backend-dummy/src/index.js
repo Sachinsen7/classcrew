666475ad0b98a7abea874b2a72e7af847583b490
Ishantp01 <163533317+Ishantp01@users.noreply.github.com>
2025-09-27 16:07:23 +0530

    login logics

--- backend-dummy/src/index.js
+++ backend-dummy/src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
 const connectDB = require("./config/db");
 
 dotenv.config({path:".env.dev"});
@@ -7,6 +8,8 @@ connectDB();
 
 const app = express();
 app.use(express.json());
+
+app.use("/api/auth", require("./modules/auth/auth.routes"));
 
 app.get("/", (req, res) => {
     res.send("class crew backend change");
---
+++ backend-dummy/src/modules/auth/auth.controller.js
@@ -0,0 +1,76 @@
+const bcrypt = require('bcryptjs');
+const User = require('../user/user.model');
+
+const jwt = require("jsonwebtoken");
+
+// Helper functions
+    const generateTokens= (user) => {
+    const accessToken = jwt.sign(
+        {id:user._id, role:user.role },
+        process.env.ACCESS_TOKEN_SECRET,
+        {expiresIn: "15m"}
+    );
+
+    const refreshToken = jwt.sign(
+        {id:user._id},
+        process.env.REFRESH_TOKEN_SECRET,
+        {expiresIn: "7d"}
+    );
+
+    return {accessToken, refreshToken};
+}
+
+
+
+exports.register = async (req, res) => {
+    try {
+        const  {name, email, password, role} = req.body;
+
+        const existingUser = await User.findOne({email});
+        if(existingUser) return res.status(400).json({message: "User already exists"});
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = new User({name, email, password: hashedPassword, role});
+
+        await user.save();
+
+        res.status(201).json({message:"User registered successfully", user: {id: user._id, name, email, role} });
+    }
+    catch(err) {
+        res.status(500).json({message: err.message});
+    }
+}
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Find user
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "Invalid credentials" });
+
+    // Check password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
+
+    // Generate tokens
+    const { accessToken, refreshToken } = generateTokens(user);
+
+    // Save refresh token in cookie
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: false, // set true in production with HTTPS
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    });
+
+    res.json({
+      message: "Login successful",
+      accessToken,
+      user: { id: user._id, name: user.name, email: user.email, role: user.role },
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
