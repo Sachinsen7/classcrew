91bd1fe8d7fd7787359c2f2f73c78f74a2a861ce
Ishantp01 <163533317+Ishantp01@users.noreply.github.com>
2025-09-27 10:59:32 +0530

    made the user modules

--- backend-dummy/src/index.js
+++ backend-dummy/src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 
-dotenv.config();
+dotenv.config({path:".env.dev"});
 connectDB();
 
 const app = express();
